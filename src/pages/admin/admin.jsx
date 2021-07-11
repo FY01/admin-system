56dd5b8f97f02edcc1d9293e0963d292ce615a91
FY01 <FY01workSpace@outlook.com>
2021-07-11 17:23:11 +0800

    20th commit, finished 404page

--- src/pages/admin/admin.jsx
+++ src/pages/admin/admin.jsx
@@ -14,6 +14,7 @@ import Home from "../home/Home";
 import Product from "../product/Product";
 import Role from "../role/Role";
 import User from "../user/User";
+import NotFound from "../not-found/not-found";
 import './admin.less'
 
 const { Footer, Sider, Content } = Layout;
@@ -33,6 +34,7 @@ class Admin extends Component {
                     <Header></Header>
                     <Content className={'content'}>
                         <Switch>
+                            <Redirect exact from='/' to = '/home'/>
                             <Route path = '/home' component = {Home}/>
                             <Route path = '/category' component = {Category}/>
                             <Route path = '/product' component = {Product}/>
@@ -41,7 +43,7 @@ class Admin extends Component {
                             <Route path = '/charts/bar' component = {Bar}/>
                             <Route path = '/charts/line' component = {Line}/>
                             <Route path = '/charts/pie' component = {Pie}/>
-                            <Redirect to = '/home'></Redirect>
+                            <Route  component = {NotFound}/>  //没有匹配到直接跳转到404页面
                         </Switch>
                     </Content>
                     <Footer style={{textAlign:"center",color:'#ccc'}}>
